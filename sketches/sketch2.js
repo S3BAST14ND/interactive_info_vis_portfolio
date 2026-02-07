e1991af1b17d352349bd529be65b28aa983a91a9
Sebastian Downes <152749018+S3BAST14ND@users.noreply.github.com>
2026-02-07 14:02:52 -0800

    added margin functions for graph area, and function to convert time to position on x/y axis

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -6,6 +6,19 @@ registerSketch('sk2', function (p) {
   //borders
   const margin = { left: 90, right: 40, top: 50, bottom: 90 };
 
+  function plotLeft() { return margin.left; }
+  function plotRight() { return W - margin.right; }
+  function plotTop() { return margin.top; }
+  function plotBottom() { return H - margin.bottom; }
+
+  function xFromMinute(min) {
+    return p.map(min, 0, 60, plotLeft(), plotRight());
+  }
+
+  function yFromHour(hr) {
+    return p.map(hr, 0, 12, plotBottom(), plotTop());
+  }
+
   p.setup = function () {
     p.createCanvas(W, H);
     p.textFont('system-ui');
@@ -14,4 +27,5 @@ registerSketch('sk2', function (p) {
   p.draw = function () {
     p.background(248);
   };
+
 });
