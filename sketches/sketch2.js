9f4faa5a1a2f3fb0ad56c27f511481d2269dee9c
Sebastian Downes <152749018+S3BAST14ND@users.noreply.github.com>
2026-02-07 13:58:28 -0800

    start of part a including registerSketch method and canvas size

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,14 +1,17 @@
 // Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
+  //size of canvas
+  const W = 800;
+  const H = 600;
+  //borders
+  const margin = { left: 90, right: 40, top: 50, bottom: 90 };
+
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(W, H);
+    p.textFont('system-ui');
   };
+
   p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    p.background(248);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
