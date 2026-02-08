c1da4b94cf76f130c7dbeefc88d13d42395a0e9f
Sebastian Downes <152749018+S3BAST14ND@users.noreply.github.com>
2026-02-07 20:14:44 -0800

    added timer state

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -3,9 +3,16 @@ registerSketch('sk4', function (p) {
   const W = 800;
   const H = 600;
 
-  // UI
+  //UI
   let minutesInput;
   let statusText = "Set minutes (max 60). Drag match to wick to light. Drag snuffer to extinguish.";
+
+  //timer state
+  let durationMs = 0;
+  let startMs = null;
+  let running = false;
+  let lit = false;
+
 
   p.setup = function () {
     p.createCanvas(W, H);
